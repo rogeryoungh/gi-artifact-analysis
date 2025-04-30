020584c0efaa584f273d99a066eed2ac5f2e2387
rogeryoungh <rogeryoungh@foxmail.com>
2025-04-30 16:15:45 +0800

    update: import primevue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, { ripple: true, theme: { preset: Aura } });
+
+app.mount('#app');
